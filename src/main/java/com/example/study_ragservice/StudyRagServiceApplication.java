959ec8cd518d56ccf77a33b7532bb63a5cd7ab84
김탁형_notebook2 <kth302303@gmail.com>
2026-08-11 09:46:44 +0900

    Openclaw - 프로젝트 연결 및 RAG 개발 셋팅완료

---
+++ src/main/java/com/example/study_ragservice/StudyRagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_ragservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRagServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRagServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/study_ragservice/StudyRagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_ragservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRagServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
